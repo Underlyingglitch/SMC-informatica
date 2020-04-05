8cae92d6573c07823cff24d512b7740fa1c273df
MasterDisaster7 <45339840+MasterDisaster7@users.noreply.github.com>
2020-04-05 14:53:36 +0200

    d

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Informatica Projecten</title>
+    <link rel="stylesheet" href="dist/css/style.css">
   </head>
   <body>
     <div class="grid-container">
