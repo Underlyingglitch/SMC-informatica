330482e1dfc5be81261f67ac7e0d88ce8d3f0972
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2020-04-05 14:06:52 +0200

    Added yahtzee

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Informatica Projecten</title>
+  </head>
+  <body>
+    <h1>Informatica Projecten</h1>
+    <ul>
+      <li><a href="yahtzee">Yahtzee</a></li>
+    </ul>
+  </body>
+</html>
