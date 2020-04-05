046a645898eb5a6bc92e39fb96db2516117c2e19
MasterDisaster7 <45339840+MasterDisaster7@users.noreply.github.com>
2020-04-05 14:32:59 +0200

    main pagina css

--- index.php
+++ index.php
@@ -5,9 +5,16 @@
     <title>Informatica Projecten</title>
   </head>
   <body>
-    <h1>Informatica Projecten</h1>
-    <ul>
-      <li><a href="5000en">5000en</a></li>
-    </ul>
+    <div class="grid-container">
+      <div class="Header"></div>
+      <div class="Login"></div>
+      <div class="Online"></div>
+      <div class="Games">
+        <h1>Informatica Projecten</h1>
+        <ul>
+          <li><a href="5000en">5000en</a></li>
+        </ul>
+      </div>
+    </div>
   </body>
 </html>
