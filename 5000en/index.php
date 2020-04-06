ee12f3553e33132055ebe2350e95d654c2d3bb1b
MasterDisaster7 <45339840+MasterDisaster7@users.noreply.github.com>
2020-04-06 11:17:31 +0200

    dobbelstenen

--- 5000en/index.php
+++ 5000en/index.php
@@ -5,7 +5,7 @@
     <title>5000en</title>
 
     <link rel="stylesheet" href="dist/css/style.css">
-
+    <script src="5000en.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   </head>
   <body>
@@ -13,6 +13,12 @@
       <div class="player">
 
       </div>
+    </div>
+    <div>
+      <div id="die1" class="dice">0</div>
+      <div id="die2" class="dice">0</div>
+      <button onclick="rollDice()">Roll Dice</button>
+      <h2 id="status" style="clear:left;"></h2>
     </div>
   </body>
 </html>
